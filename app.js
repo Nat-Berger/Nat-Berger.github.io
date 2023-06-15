a0f27b16479c5906e8d5a14381fb396ccbab820c
nat-berger <nathanielberger1998@gmail.com>
2023-06-15 19:34:22 +0100

    Added functionality to move items to completed in local storage

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ function deleteCheck(e) {
     const todo = item.parentElement;
     todo.classList.add("fall");
     removeLocalTodos(todo);
+    removeLocalCompletedTodos(todo);
     todo.addEventListener("transitionend", function () {
       todo.remove();
     });
@@ -54,6 +55,7 @@ function deleteCheck(e) {
     const todo = item.parentElement;
     todo.classList.toggle("completed");
     saveLocalCompletedTodos(todo.innerText);
+    removeLocalTodos(todo);
   }
 }
 
@@ -94,10 +96,9 @@ function saveLocalTodos(todo) {
   localStorage.setItem('todos', JSON.stringify(todos));
 }
 
-
 function saveLocalCompletedTodos(todoText) {
   let completed;
-  
+
   if (localStorage.getItem('completed') === null) {
     completed = [];
   } else {
@@ -114,51 +115,76 @@ function saveLocalCompletedTodos(todoText) {
 
 /*
   When clicking the save todos: I want to be able to:
-  1. Add the saved item to the Completed list in local storage 
+  1. Add the saved item to the Completed list in local storage      DONE
 
-  When unchecking the completed list, needs to be removed from the 'completed' list.
+  When unchecking the completed list, needs to be removed from the 'completed' list but put back into the todos!.    PENDING
   
   When refreshing the page, the 'get todos' function must return all, 
-  but then we need a new function to set styling based off the list shown (completed list)
-
-   
+  but then we need a new function to set styling based off the list shown (completed list)   DONE
 
 
 */
 
 function getTodos() {
   let todos;
+  let completed;
   if (localStorage.getItem('todos') === null) {
     todos = [];
-  } else {
-    todos = JSON.parse(localStorage.getItem('todos'));
+    console.log('todos is empty');
   }
+  else {
+    const completed = JSON.parse(localStorage.getItem('completed'));
+    completed.forEach(function (todo) {
+      const todoDiv = document.createElement('div');
+      todoDiv.classList.add("todo");
+      //create list
+      const newTodo = document.createElement('li');
+      newTodo.innerText = todo;
+      newTodo.classList.add('todo-item');
+      todoDiv.appendChild(newTodo);
 
-  todos.forEach(function (todo) {
-    const todoDiv = document.createElement('div');
-    todoDiv.classList.add("todo");
-    //create list
-    const newTodo = document.createElement('li');
-    newTodo.innerText = todo;
-    newTodo.classList.add('todo-item');
-    todoDiv.appendChild(newTodo);
+      //check button
+      const completedButton = document.createElement('button');
+      completedButton.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+      completedButton.classList.add("complete-btn");
+      todoDiv.appendChild(completedButton);
+      //delete button
+      const deleteButton = document.createElement('button');
+      deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+      deleteButton.classList.add("delete-btn");
+      todoDiv.appendChild(deleteButton);
+      //append todo
+      todoList.appendChild(todoDiv);
+      todoDiv.classList.toggle('completed');
+    })
+    const todos = JSON.parse(localStorage.getItem('todos'));
+    todos.forEach(function (todo) {
+      const todoDiv = document.createElement('div');
+      todoDiv.classList.add("todo");
+      //create list
+      const newTodo = document.createElement('li');
+      newTodo.innerText = todo;
+      newTodo.classList.add('todo-item');
+      todoDiv.appendChild(newTodo);
 
-    //check button
-    const completedButton = document.createElement('button');
-    completedButton.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
-    completedButton.classList.add("complete-btn");
-    todoDiv.appendChild(completedButton);
-    //delete button
-    const deleteButton = document.createElement('button');
-    deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
-    deleteButton.classList.add("delete-btn");
-    todoDiv.appendChild(deleteButton);
-    //append todo
-    todoList.appendChild(todoDiv);
-   
-  })
+      //check button
+      const completedButton = document.createElement('button');
+      completedButton.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+      completedButton.classList.add("complete-btn");
+      todoDiv.appendChild(completedButton);
+      //delete button
+      const deleteButton = document.createElement('button');
+      deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+      deleteButton.classList.add("delete-btn");
+      todoDiv.appendChild(deleteButton);
+      //append todo
+      todoList.appendChild(todoDiv);
+      console.log('Successfully added all of the items');
+    })
+  }
 }
-function removeLocalTodos(todo){
+
+function removeLocalTodos(todo) {
   let todos;
   if (localStorage.getItem('todos') === null) {
     todos = [];
@@ -168,4 +194,15 @@ function removeLocalTodos(todo){
   const todoIndex = todo.children[0].innerText;
   todos.splice(todos.indexOf(todoIndex), 1);
   localStorage.setItem("todos", JSON.stringify(todos));
+}
+function removeLocalCompletedTodos(todo) {
+  let completed;
+  if (localStorage.getItem('completed') === null) {
+    completed = [];
+  } else {
+    completed = JSON.parse(localStorage.getItem('completed'));
+  }
+  const todoIndex = todo.children[0].innerText;
+  completed.splice(completed.indexOf(todoIndex), 1);
+  localStorage.setItem("completed", JSON.stringify(completed));
 }
